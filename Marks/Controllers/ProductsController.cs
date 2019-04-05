298516ee89288b2ba6c51a891960601319c34b18
vatergo <vatergo@mail.ru>
2019-04-05 22:36:37 +0500

    Add controller

--- Marks/Controllers/ProductsController.cs
+++ Marks/Controllers/ProductsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,11 +13,18 @@ namespace Marks.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+        private EFDBContext context;
+
+        public ProductsController(EFDBContext context)
+        {
+            this.context = context;
+        }
+
         // GET: api/<controller>
         [HttpGet]
-        public IEnumerable<string> GetProducts()
+        public ActionResult<Product> GetProducts()
         {
-            return new string[] { "value1", "value2" };
+            return Ok();
         }
 
         // GET api/<controller>/5
